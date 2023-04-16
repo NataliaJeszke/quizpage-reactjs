df70a3e9f423d7e1d83cd9c0398023f5c3276fc3
NataliaJeszke <nataliajeszke8@gmail.com>
2023-04-16 19:51:51 +0200

    added responsive img

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -3,13 +3,19 @@ import { CategoryHTML } from "./CategoriesCards/CategoryHTML/CategoryHTML";
 import { CategoryJS } from "./CategoriesCards/CategoryJS/CategoryJS";
 import { Link } from "react-router-dom";
 import style from "./WelcomeStyle.module.css";
+import image from '../../assets/programmer.png';
 
 export function Welcome() {
   return (
     <>
       <header className={style.header}>
+        <div className="header_container">
         <h1>Are you fit for Junior Frontend Developer?</h1>
         <h4>Check your knowlage, choose category</h4>
+        </div>
+        <div>
+      <img src={image} alt="Opis obrazka" />
+    </div>
       </header>
       <div className={style.categories_cards}>
         <Link to="/questions">
