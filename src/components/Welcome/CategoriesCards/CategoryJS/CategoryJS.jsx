6959e881245ffc4cbf33b7fbb549b79f5f016714
NataliaJeszke <nataliajeszke8@gmail.com>
2023-04-16 20:19:36 +0200

    working onclick for JS category

--- src/components/Welcome/CategoriesCards/CategoryJS/CategoryJS.jsx
+++ src/components/Welcome/CategoriesCards/CategoryJS/CategoryJS.jsx
@@ -1,8 +1,11 @@
 
 import style from '../../CategoriesCards/CategoriesCardsStyle.module.css'
 export function CategoryJS(){
+    function handleClick() {
+        console.log('hello world JS');
+      }
     return(
-        <div className={style.card_one}>
+        <div className={style.card_one} onClick={handleClick}>
             <h3>JavaScript</h3>
             <p>Check your basic knowlage of Pure JS</p>
         </div>
