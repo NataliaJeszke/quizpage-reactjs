13b8bcc6333ce1ba7162539e8eab5c35ac00049c
NataliaJeszke <nataliajeszke8@gmail.com>
2023-04-18 10:05:27 +0200

    deleted comment

--- src/components/Welcome/CategoriesCards/CategoryJS/CategoryJS.jsx
+++ src/components/Welcome/CategoriesCards/CategoryJS/CategoryJS.jsx
@@ -2,11 +2,8 @@ import style from "../../CategoriesCards/CategoriesCardsStyle.module.css";
 import { useState } from "react";
 
 export function CategoryJS() {
-//   const [selectedQuiz, setSelectedQuiz] = useState("");
   return (
-    <div
-      className={style.card_one}
-    >
+    <div className={style.card_one}>
       <h3>JavaScript</h3>
       <p>Check your basic knowlage of Pure JS</p>
     </div>
