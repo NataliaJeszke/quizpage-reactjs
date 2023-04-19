ea22604df0a05c87ca2e38e46824176348892447
NataliaJeszke <nataliajeszke8@gmail.com>
2023-04-19 12:56:42 +0200

    code refactor

--- src/components/Welcome/CategoriesCards/CategoryCSS/CategoryCSS.jsx
+++ src/components/Welcome/CategoriesCards/CategoryCSS/CategoryCSS.jsx
@@ -1,8 +1,9 @@
-import style from '../../CategoriesCards/CategoriesCardsStyle.module.css'
-export function CategoryCSS(){
-    return(
-        <div className={style.card_one}>
-            <h3>CSS</h3>
-        </div>
-    )
+import style from "../../CategoriesCards/CategoriesCardsStyle.module.css";
+
+export function CategoryCSS() {
+  return (
+    <div className={style.card_one}>
+      <h3>CSS</h3>
+    </div>
+  );
 }
